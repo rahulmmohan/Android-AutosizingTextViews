357126d6ebcc769050a7f95009a6462e43390ca3
rahul.m <rahulm@ivedix.com>
2017-04-05 18:52:43 +0530

    auto size attributes

--- app/src/main/java/com/overrideandroid/autosizingtextviews/MainActivity.java
+++ app/src/main/java/com/overrideandroid/autosizingtextviews/MainActivity.java
@@ -2,6 +2,10 @@ package com.overrideandroid.autosizingtextviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((EditText) findViewById(R.id.editText)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                ((TextView) findViewById(R.id.text)).setText(editable.toString());
+            }
+        });
     }
 }
